70ec76b70052c0cbf64d102a7b985d939c1d58ad
James Hodgkinson <jhodgkinson@splunk.com>
2025-08-01 13:53:23 +1000

    linting tests

---
+++ tests/test_able_table.py
@@ -0,0 +1,13 @@
+from able_assistant import able_table
+
+
+async def test_able_table() -> None:
+    # Test the basic functionality of able_table
+    table = await able_table(
+        hypothesis="Test Hypothesis",
+        research_document="Test Research Document",
+        local_context="Test Local Context",
+        previous_run=[],
+    )
+    assert table is not None
+    print(table)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from utils import find_dotenv_file
 
 
-def test_find_dotenv_file():
+def test_find_dotenv_file() -> None:
     """Test that find_dotenv_file correctly identifies the .env file"""
     # Assuming the test is run in a directory where a .env file exists
     env_file = find_dotenv_file()
