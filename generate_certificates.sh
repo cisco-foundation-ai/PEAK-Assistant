bdedb2bc9fffee428f1c9ce37c595bc5533d4251
James Hodgkinson <jhodgkinson@splunk.com>
2025-08-01 17:32:24 +1000

    setting the subject in generate_certficates

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -4,6 +4,6 @@ if [ -f UI/cert.pem ] || [ -f UI/key.pem ]; then
     echo "SSL certificates already exist. Skipping generation."
 else
     echo "Generating self-signed SSL certificates..."
-    openssl req -x509 -newkey rsa:4096 -keyout UI/key.pem -out UI/cert.pem -days 365 -nodes
+    openssl req -x509 -newkey rsa:4096 -keyout UI/key.pem -out UI/cert.pem -days 365 -nodes -subj "/CN=localhost"
     echo "SSL certificates generated and saved in UI directory."
 fi
